5b427305f611f7c867a4934b0391d7ca794c995e
Pierre Barbé <barb.pierre@yahoo.fr>
2024-04-14 23:24:30 +0200

    departement check

--- app/csv test.py
+++ app/csv test.py
@@ -1,5 +1,6 @@
 import csv
+import math
 with open('app/communes-departement-region.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['code_postal'], row['nom_commune_complet'].upper())
+        print(row['code_postal'], row['nom_commune_complet'].upper(), math.floor(int(row['code_postal'])/1000))
